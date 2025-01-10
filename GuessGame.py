d2585ae8e860187be5df201730d1954a4650d89f
Abdul Rafay <156508072+mrsiuuuux1@users.noreply.github.com>
2025-01-11 00:34:35 +0500

    Add files via upload

---
+++ GuessGame.py
@@ -0,0 +1,14 @@
+answer = 9
+No_Of_Attempts = 1
+
+while No_Of_Attempts <= 3:
+    guess = int(input("Enter a number within the range (1-15): "))  # Get user input inside the loop
+    if guess == answer:
+        print("You Won")
+        break
+    else:
+        No_Of_Attempts += 1
+        if No_Of_Attempts <= 3:
+            print(f"Sorry, you guessed the wrong number :(")
+        else:
+            print("You have no more attempts left! BETTER LUCK NEXT TIME!!!!")
